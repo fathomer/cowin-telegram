8741ba0cb96a9e2df76ea750f5f79da5a8cb18ff
fathomer <42073497+fathomer@users.noreply.github.com>
2021-05-04 19:09:12 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import json
 
 
 response = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=192&date=05-05-2021")
-jsonresponse = json.loads(json.loads(response.text))
+# jsonresponse = json.loads(json.loads(response.text))
 print("Response")
 print("=================")
-print(jsonresponse)
+print(response.text)
