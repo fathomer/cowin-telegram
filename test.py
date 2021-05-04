ebaf01f433b72cffba71399c40bade378ed11fa5
fathomer <42073497+fathomer@users.noreply.github.com>
2021-05-04 19:07:06 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+
+response = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=192&date=05-05-2021")
+jsonresponse = json.loads(json.loads(response.text))
+print("Response")
+print("=================")
+print(jsonresponse)
