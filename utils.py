ad8ba81ee6a83e05bb008fe4fe65acc925b7667f
fathomer <kartikabc712@gmail.com>
2021-06-28 06:41:15 +0000

    Fix message

--- utils.py
+++ utils.py
@@ -3,17 +3,24 @@ from datetime import datetime
 
 def formatDictToMessage(input_dict):
     availabilty = ""
-    if input_dict['available_capacity_dose1'] > 0:
-        availabilty+=f"\nDOSE 1: {input_dict['available_capacity_dose1']} Available"
-    if input_dict['available_capacity_dose2'] > 0:
-        availabilty+=f"\nDOSE 2: {input_dict['available_capacity_dose2']} Available"
+    if input_dict.get('available_capacity_dose1'):
+        availabilty+=f"\nDOSE 1: {input_dict.get('available_capacity_dose1')} Available"
+    if input_dict.get('available_capacity_dose2'):
+        availabilty+=f"\nDOSE 2: {input_dict.get('available_capacity_dose2')} Available"
 
     output = f"Date: {getFormattedDate(input_dict['date'])}\n" \
              f"Center Name: {input_dict['name']}\n" \
              f"Vaccine: {input_dict['vaccine']}" \
              f"{availabilty}\n" \
-             f"<pre>Address: {input_dict['address']} - {input_dict['pincode']}</pre>"
+             f"<pre>Address: {getAddressOrCity(input_dict)} - {input_dict['pincode']}</pre>"
     return output
+
+def getAddressOrCity(inputDict):
+    if inputDict.get('address'):
+        return inputDict.get('address')
+    if inputDict.get('block_name'): 
+        return inputDict.get('block_name')
+    return ''
 
 def getFormattedDate(date):
     return datetime.strptime(date, '%d-%m-%Y').strftime('%e %B %y')
